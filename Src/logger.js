91b718f596143011a532056a40b1a2a34ca9581e
shabakoketso <shaba.koketso@gmail.com>
2026-08-07 19:33:12 +0200

    Create logger.js

---
+++ Src/logger.js
@@ -0,0 +1,5 @@
+    module.exports = { 
+      info: (msg) => console.log(`[INFO] ${msg}`), 
+      error: (msg) => console.error(`[ERROR] ${msg}`), 
+      warn: (msg) => console.warn(`[WARN] ${msg}`) 
+    }
